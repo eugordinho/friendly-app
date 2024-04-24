39463c1f544738597b98dadd19188aedfda9cd6e
eugordinho <martinsanchezr@outlook.com>
2024-04-24 17:52:21 -0300

    Adding Backend

---
+++ clearfriendships/src/main/java/com/mfsr/clearfriendships/ClearfriendshipsApplication.java
@@ -0,0 +1,13 @@
+package com.mfsr.clearfriendships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearfriendshipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearfriendshipsApplication.class, args);
+	}
+
+}
